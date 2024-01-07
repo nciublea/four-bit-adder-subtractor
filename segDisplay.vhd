d430d5234424949fd660ffee5b75ae93d59f846a
Nicholas Ciublea <116814758+nciublea@users.noreply.github.com>
2024-01-07 14:51:06 -0500

    decoder that outputs to 7-segment display

---
+++ segDisplay.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Module Name:    segDisplay - Behavioral 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity segDisplay is
+    Port ( x0 : in  STD_LOGIC;
+           x1 : in  STD_LOGIC;
+           x2 : in  STD_LOGIC;
+           x3 : in  STD_LOGIC;
+           a : out  STD_LOGIC;
+           b : out  STD_LOGIC;
+           c : out  STD_LOGIC;
+           d : out  STD_LOGIC;
+           e : out  STD_LOGIC;
+           ff : out  STD_LOGIC;
+           g : out  STD_LOGIC);
+end segDisplay;
+
+architecture Behavioral of segDisplay is
+
+begin
+
+a <= (not x0 and x1 and not x2 and not x3) or (not x0 and not x1 and not x2 and x3) or (x0 and x1 and not x2 and x3) or (x0 and not x1 and x2 and x3);
+
+b <= (not x0 and x1 and not x2 and x3) or (not x0 and x1 and x2 and not x3) or (x0 and not x1 and x2 and x3) or (x0 and x1 and not x2 and not x3) or (x0 and x1 and x2 and not x3) or (x0 and x1 and x2 and x3);
+
+c <= (not x0 and not x1 and x2 and not x3) or (x0 and x1 and not x2 and not x3) or (x0 and x1 and x2 and not x3) or (x0 and x1 and x2 and x3);
+
+d <= (not x0 and not x1 and not x2 and x3) or (not x0 and x1 and not x2 and not x3) or (not x0 and x1 and x2 and x3) or (x0 and not x1 and x2 and not x3) or (x0 and x1 and x2 and x3);
+
+e <= (not x0 and not x1 and not x2 and x3) or (not x0 and not x1 and x2 and x3) or (not x0 and x1 and not x2 and not x3) or (not x0 and x1 and x2 and x3) or (x0 and not x1 and not x2 and x3) or (not x0 and x1 and not x2 and x3);
+
+ff <= (not x0 and not x1 and not x2 and x3) or (not x0 and not x1 and x2 and not x3) or (not x0 and not x1 and x2 and x3) or (not x0 and x1 and x2 and x3) or (x0 and x1 and not x2 and x3);
+
+g <= (not x0 and not x1 and not x2 and not x3) or (not x0 and not x1 and not x2 and x3) or (not x0 and x1 and x2 and x3) or (x0 and x1 and not x2 and not x3);
+
+end Behavioral;
